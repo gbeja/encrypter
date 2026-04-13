2b6f5e0ac12dc5504534710940088ae2579a17c1
gbeja <seniormanjosh@gmail.com>
2026-04-13 01:58:22 +0100

    Add files via upload

---
+++ Testing Combinations.py
@@ -0,0 +1,8 @@
+meal=["pizza","burger","chicken"]
+drink=["coke","fanta","sprite"]
+dessert=["ice cream","cake","donut"]
+
+for m in meal:
+    for d in drink:
+        for ds in dessert:
+            print("I want to eat "+m+" and drink "+d+" and eat "+ds)
